337bcb8df2bc4271e1052fffba64876bc13f233a
Roman <127163323+zodackwand@users.noreply.github.com>
2023-06-06 11:44:13 -0700

    second

--- main.py/main.py
+++ main.py/main.py
@@ -1,1 +1,21 @@
-print('Hello World')
+import sqlite3
+
+conn = sqlite3.connect('database_todolist.db')
+
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS assignments("
+               "id INTEGER PRIMARY KEY,"
+               "name TEXT,"
+               "class TEXT)"
+               "")
+
+#cursor.execute("ALTER TABLE assignments ADD done BOOLEAN")
+
+def new_assignment(name=0, class_name=0, deadline=0, done=False):
+    cursor.execute("INSERT INTO assignments (name, class, deadline, done) VALUES (?, ?, ?, ?)", (name, class_name, deadline, done))
+
+
+
+
+conn.commit()
+conn.close()
